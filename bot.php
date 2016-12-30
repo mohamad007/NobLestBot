4cbd0dcddd2a9c10d27927686862cd0f2ac3bd75
mohamad007 <mohamad007@users.noreply.github.com>
2016-12-30 08:00:53 -0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 
-define('API_KEY','');
+define('API_KEY','277064935:AAHusd1YO-ORm3Vgr5vOyaZxozbNDu6UPY8');
 //----######------
 function makereq($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
@@ -63,7 +63,7 @@ $file = $update->message->document;
 $music = $update->message->audio;
 $voice = $update->message->voice;
 $forward = $update->message->forward_from;
-$admin = 193930120;
+$admin = 266151360;
 //-------
 function SendMessage($ChatId, $TextMsg)
 {
@@ -182,7 +182,7 @@ elseif($textmessage == 'درباره من')
 
 	elseif($textmessage == 'شماره من')
 {
-	$phone = '+98';
+	$phone = '+989381699829';
 	$namea = 'Dev';
 makereq('sendContact',[
 	'chat_id'=>$chat_id,
